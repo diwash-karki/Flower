b3e9365ab258969e17fda5cff2a048c56d317060
diwash-karki <diwashkarki7338@gmail.com>
2024-05-22 01:28:00 -0500

    new

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
+      document.getElementById("audio").play();  
       clearTimeout(c);
     }, 1000);
   };
+
+
+function startAnimation(){
+  document.getElementById("hidden").style.display = "block";
+  document.getElementById("hidden").style.animation = "fadeIn 1s";
+  document.getElementById("hidden").style.animationFillMode = "forwards";
+  document.getElementById("hidden").style.animationDelay = "1s";
+  document.getElementById("hidden").style.animationIterationCount = "1";
+
+}
